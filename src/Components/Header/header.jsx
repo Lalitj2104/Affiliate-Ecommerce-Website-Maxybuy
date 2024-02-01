e70d5bda7dd72fae834d0eed9461e9933298d474
Lalit Max <lalitmay@gmail.com>
2024-02-01 14:59:07 +0530

    remove normal css and link tailwindcss

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useState } from 'react'
-import './header.css'
 import { NavLink } from 'react-router-dom'
 import { StoreContext } from '../Store/storeContext'
 import AllImgList from '../../assets/all_img_list'
@@ -7,10 +6,10 @@ const Header = () => {
 
   const { menu, setMenuState,items } = useContext(StoreContext)
   return (
-    <header className='d'>
-      <div className='navbar px-5'>
-        <NavLink to='/' onClick={() => setMenuState('shop')} className='logo'>Logo</NavLink>
-        <ul className='nav-links sm:visible  invisible'>
+    <header className='w-full flex  flex-col items-center select-none fixed top-3'>
+      <div className='navbar-box-shadow-css h-[75px] flex items-center justify-between rounded-2   border-solid border-gray-800 p-4 px-5'>
+        <NavLink to='/' onClick={() => setMenuState('shop')} className='logo text-[#fff] cursor-pointer text-[30px] font-bold'>Logo</NavLink>
+        <ul className='flex w-2/5 justify-between text-[#fff] sm:visible invisible'>
           <li><NavLink to='/' onClick={() => setMenuState('shop')} href="#">Shop</NavLink> {menu === 'shop' ? <hr /> : ""}</li>
           <li><NavLink to='men' onClick={() => setMenuState('men')} href="#">Men</NavLink>  {menu === 'men' ? <hr /> : ""}</li>
           <li><NavLink to='women' onClick={() => setMenuState('women')} href="#">Women</NavLink> {menu === 'women' ? <hr /> : ""}</li>
@@ -19,13 +18,13 @@ const Header = () => {
 
         </ul>
 
-        <div className="login-sign-up s sm:gap-14  ">
-          <NavLink onClick={() => setMenuState('login')} to='login' className='login h-9'>Login</NavLink>
+        <div className="flex gap-x-4  items-center  justify-between h-10 mr-[30px] rounded-md sm:gap-14  ">
+          <NavLink onClick={() => setMenuState('login')} to='login' className='login h-[35px] text-[18px] w-[70px]  border-solid border-[1px] border-white  text-white p-[5px] flex  items-center   justify-center rounded-md hover:bg-[#223e6f] '  >Login</NavLink>
 
 
-          <NavLink to='cartContainer' className="cart ">
-            <span className='countItem'>{items.length}</span>
-            <img src={AllImgList.cartIcon} alt="cart img" />
+          <NavLink to='cartContainer' className="h-[30px] w-[40px] sm:h-[40px] sm:w-[50px] flex  items-center  justify-center  bg-[#fff] rounded-md  relative ">
+            <span className='absolute top-[-13px] left-4/12 p-1 border-solid  border-[1px] border-white w-4 h-4 flex  items-center justify-center  bg-green-400 roun rounded-full'>{items.length}</span>
+            <img className='w-4/6' src={AllImgList.cartIcon} alt="cart img" />
           </NavLink>
 
         </div>
--- src/Components/Pages/Home/home.jsx
+++ src/Components/Pages/Home/home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './home.css'
 import heroImage from '../../../assets/hero_image.png'
 import arrow from '../../../assets/arrow.png'
 import HomeSection from './homeSection'
@@ -11,26 +10,26 @@ const Home = () => {
   console.log(data_product)
   return (<>
 
-    <section className='hero-section'>
+    <section className='full-page-section bg-slate-500'>
 
-      <div className="hero">
-        <div className="hero-left">
-          <div className="slogan">
-            <h1>Evevate Your Style</h1>
+      <div className="centre-page-section h-[900px]">
+        <div className="hero-left h-2/5 flex flex-col justify-around pl-[50px]">
+          <div>
+            <h1 className='text-[#2e2626]'>Evevate Your Style</h1>
           </div>
-          <div className="slogan-description">
-            <h1>New <br />Collections <br />for Everyone</h1>
+          <div >
+            <h1 className='text-[60px]'>New <br />Collections <br />for Everyone</h1>
           </div>
 
-          <div className="go-latest">
-            <button>Latest Collectons <img src={arrow} alt="arrow" /></button>
+          <div className="go-latest w-full m-auto">
+            <button className=' text-[25px] cursor-pointer h-[50px] font font-bold rounded-md px-[10px] text-white border-solid border-2  border-white bg-[#08051f]'>Latest Collectons <img src={arrow} alt="arrow" /></button>
           </div>
 
 
         </div>
 
-        <div className="hero-right">
-          <img className='hero-img' src={heroImage} alt="hero-img" />
+        <div className=" h-3/5 w-2/5  flex items-center justify-center ">
+          <img className='w-full' src={heroImage} alt="hero-img" />
 
         </div>
 
